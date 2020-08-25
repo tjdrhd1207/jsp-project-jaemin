7dbdb94a0d42387ed11ab3cf5a4498673c0e73c0
whlthd1207 <whlthd1207@naver.com>
2020-08-25 12:37:01 +0900

    서비스 dao 에러페이지 작성

---
+++ src/com/kh/jsp/member/model/dao/MemberDao.java
@@ -0,0 +1,5 @@
+package com.kh.jsp.member.model.dao;
+
+public class MemberDao {
+
+}
--- src/com/kh/jsp/member/model/service/MemberService.java
+++ src/com/kh/jsp/member/model/service/MemberService.java
@@ -1,12 +1,23 @@
 package com.kh.jsp.member.model.service;
 
+import java.sql.Connection;
+
+import com.kh.jsp.member.model.dao.MemberDao;
 import com.kh.jsp.member.model.vo.Member;
 
 public class MemberService {
 
 	public Member loginCheck(Member requestMember) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection con =getConnection();
+		
+		Member loginUser = new MemberDao().loginCheck(con,requestMember);
+		
+		close(con);
+		
+		return loginUser;
 	}
 
+	
+	
 }
+
